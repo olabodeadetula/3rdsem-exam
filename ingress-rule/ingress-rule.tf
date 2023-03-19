65eb73c40b09c9a83898a6f27d5c31324785ddd4
Olabode Adetula <olabodeadetula@yahoo.com>
2023-03-19 22:22:24 +0000

    exam-project

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,61 @@
+resource "kubernetes_ingress_v1" "voting-ingress" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "vote.olabodeadetula.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-app"
+    namespace = "sock-app"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "socks-shop.olabodeadetula.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
