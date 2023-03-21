7b6f77a467530b811551f45df62b822077deeb28
Olabode Adetula <olabodeadetula@yahoo.com>
2023-03-21 04:23:24 +0000

    exam-project

--- eks/iam-test.tf
+++ eks/iam-test.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
 
 resource "aws_iam_role" "test_oidc" {
   assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
-  name               = "test-oidc"
+  name               = "iamtest-oidc"
 }
 
 resource "aws_iam_policy" "test-policy" {
