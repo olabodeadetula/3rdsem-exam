fc0f846b28d3927bef173d2b266d86087fbddaad
Olabode Adetula <olabodeadetula@yahoo.com>
2023-03-22 12:09:52 +0000

    exam-project

--- nginx-controller/helm-nginx.tf
+++ nginx-controller/helm-nginx.tf
@@ -11,7 +11,7 @@ resource "kubernetes_namespace" "nginx-namespace" {
 
   depends_on = [time_sleep.wait_for_kubernetes]
   metadata {
-    name = "nginx-ingress"
+    name = "nginxingress"
   }
 }
 
